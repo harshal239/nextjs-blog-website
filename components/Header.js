0723d4640846adc5fef1e7110045838951a2496d
harshal239 <harshalwalunj49@gmail.com>
2022-02-08 20:21:02 +0530

    Final Updates

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-[rgb(70,130,180)]">
       <div className="max-w-5xl w-11/12 mx-auto py-4 text-white mb-5">
-        <Link href="/">
+        <Link href="/" passHref>
           <h2 className="text-2xl font-semibold">Dev Blog</h2>
         </Link>
       </div>
